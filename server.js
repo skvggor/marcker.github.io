36368880c31dc9f1d915ffc9e2365a5a006dc55f
Marcos Garcia de Lima <marckfree@gmail.com>
2018-03-29 14:48:59 -0300

    Add public (root)

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const site = express();
 const port = process.env.PORT || 8080
 
-site.use(express.static('./public'));
+site.use(express.static('public'));
 
 site.set('view engine', 'ejs');
 site.get('/', (request, response) => response.render('pages/index'));
