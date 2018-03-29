690867938edde06e555094f5f31a8d9f5a7c180c
Marcos Garcia de Lima <marckfree@gmail.com>
2018-03-29 14:37:58 -0300

    Add CSS

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const site = express();
 const port = process.env.PORT || 8080
 
+site.use(express.static('public'));
 site.set('view engine', 'ejs');
 site.get('/', (request, response) => response.render('pages/index'));
 site.listen(port);
